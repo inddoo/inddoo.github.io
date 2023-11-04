b721a5fa51da9dd45dca2723e68c9bd6a52e6db5
inddoo <watashiindonesiana@gmail.com>
2023-11-04 09:40:41 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// animated
+var typed = new Typed(".typing", {
+    strings:["web Designer","web Developer","Graphic Designer","Youtuber"],
+    typedSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
